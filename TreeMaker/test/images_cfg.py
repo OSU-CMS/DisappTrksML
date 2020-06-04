8c723a62b2a20fcdc21cfec93b7899ff39263423
Brian Francis <brian.patrick.francis@cern.ch>
2020-06-04 10:38:18 -0400

    Update input file list

--- TreeMaker/test/images_cfg.py
+++ TreeMaker/test/images_cfg.py
@@ -25,7 +25,19 @@ process.maxEvents = cms.untracked.PSet (
 )
 process.source = cms.Source ("PoolSource",
     fileNames = cms.untracked.vstring (
-    	'/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/00001/64649185-CAF3-E711-955D-02163E014685.root',
+        # M>50
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80001/8AD8A719-03FE-E711-BBE9-001E677925A0.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80001/E89A4682-03FE-E711-A8BF-001E67E6F990.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80001/CE2DD2B6-ABFE-E711-B66A-0242AC130002.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80001/4ED9C9CC-DBFE-E711-A64A-0242AC130002.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80001/E04706CD-8AFE-E711-85C1-0CC47A6C1054.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80001/8C336D9E-8DFE-E711-981A-0CC47A13D3B2.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80001/0040D871-CEFE-E711-8A3B-0CC47A2B04CC.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80001/CA38873C-E2FE-E711-BD3B-003048F5B69A.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80000/5693479A-01FE-E711-972A-BC305B3909FE.root',
+        '/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/80000/74738E96-01FE-E711-AB3C-BC305B390A59.root',
+
+    	#'/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/RECOSIMstep_94X_mc2017_realistic_v10_ext1-v1/00001/64649185-CAF3-E711-955D-02163E014685.root',
         #'/store/mc/RunIIFall17DRPremix/DYJetsToLL_M-5to50_TuneCP5_13TeV-madgraphMLM-pythia8/AODSIM/PU2017_94X_mc2017_realistic_v11-v3/280000/0002944B-672B-E911-8E3C-0025905B85CC.root',
     ),
 )
