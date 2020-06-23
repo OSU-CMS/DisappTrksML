0e621294a394f48297ced179e2f771af7530c978
carriganm95 <carriganm95@gmail.com>
2020-06-23 11:27:42 -0400

    bug fixes

--- autoencoder/reduceNoiseV2.py
+++ autoencoder/reduceNoiseV2.py
@@ -18,7 +18,7 @@ batches = 16
 
 #set directories
 workDir = "/home/mcarrigan/disTracksML/autoencoder/"
-saveDir = "/home/mcarrigan/disTracksML/data/v2/cleaned/"
+saveDir = "/data/disappearingTracks/cleaned/"
 dataDir = "/home/mcarrigan/disTracksML/data/v2/"
 
 weights = "/home/mcarrigan/disTracksML/weights/autoencoder/W_0p25_tanh.h5"
@@ -33,9 +33,9 @@ ae = Conv2D(32, (3,3), activation = 'relu', padding = 'same')(encoded)
 ae = UpSampling2D((2,2))(ae)
 decoded = Conv2D(1, (3,3), activation = 'sigmoid', padding = 'same')(ae)
 autoencoder = Model(img, decoded)
+autoencoder.load_weights(weights)
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 #autoencoder.fit(noisyTrain, x_train, epochs = num_epochs, batch_size = batches, shuffle=True, validation_data = (noisyVal, x_val))
-autoencoder.load_weights(weights)
 print("ok")
 
 
@@ -45,15 +45,25 @@ def reshapeData(df):
     df_m = df.loc[df['type'] == 2]
     data_e = df_e.iloc[:, 4:].to_numpy()
     data_e = np.reshape(data_e, (data_e.shape[0], 40, 40, 4))
-    data_e = data_e[:, :, :, :]
+    data_e = data_e[:, :, :, [0, 2, 3]]
     data_bkg = df_bkg.iloc[:, 4:].to_numpy()
     data_bkg = np.reshape(data_bkg, (data_bkg.shape[0],40,40,4))
-    data_bkg = data_bkg[:, :, :, :]
+    data_bkg = data_bkg[:, :, :, [0,2,3]]
     data_m = df_m.iloc[:, 4:].to_numpy()
     data_m = np.reshape(data_m, (-1, 40, 40, 4))
-    data_m = data_m[:, :, :, :]
+    data_m = data_m[:, :, :, [0,2,3]]
     data = [data_bkg, data_e, data_m]
-    for i in range(len(data)):
+    fig, plot = plt.subplots(1,5, figsize = (10,5))
+    #for j in range(5):
+        #rand = np.random.randint(0, data[1].shape[0])
+        #plot[j].imshow(data[1][rand, :, :, 0])
+        #plt.savefig(workDir + 'ReshapedImg.png')
+
+    return data
+
+
+def clean(data):
+    for i in range(3):
         x_test = data[i]
         #print(x_test.shape[0])
         if(x_test.shape[0] == 0): continue
@@ -61,11 +71,23 @@ def reshapeData(df):
         #reshape data to be 4 dimensions for nn
         x_ECAL = np.reshape(x_ECAL, (-1, 40, 40, 1))
         denoised = autoencoder.predict(x_ECAL)
-        denoised = np.reshape(denoised, (-1, 40, 40))
-        denoised = np.array([denoised, x_test[:, :, :, 1], x_test[:, :, :, 2], x_test[:, :, :, 3]])
-        denoised = np.reshape(denoised, (-1, 40, 40, 4))
-        #print(denoised.shape)
-        data[i] = denoised
+        #denoised = np.reshape(denoised, (-1, 40, 40))
+        hcal = np.reshape(x_test[:, :, :, 1], (-1, 40, 40, 1))
+        muons = np.reshape(x_test[:, :, :, 2], (-1, 40, 40, 1))
+        cleaned = np.concatenate((denoised, hcal, muons), axis=3)
+        #cleaned = np.reshape(cleaned, (-1, 40, 40, 3))
+        print("denoised shape", denoised.shape)
+        print("clean shape", cleaned.shape)
+        data[i] = cleaned
+        #print("data[i] shape", data[i].shape)
+        #if i == 1:
+            #fig, plot = plt.subplots(3,5, figsize = (8,5))
+            #for j in range(5):
+                #rand = np.random.randint(0, cleaned.shape[0])
+                #plot[0,j].imshow(denoised[rand, :, :,0])
+                #plot[1,j].imshow(cleaned[rand, :, :, 0])
+                #plot[2,j].imshow(x_ECAL[rand, :, :, 0])
+            #plt.savefig(workDir + 'CleanedImg.png')
     return data
 
 
@@ -80,13 +102,16 @@ def getAttributes(df):
     return att
    
 
-def shuffleData(data, classes):
+def shuffleData(data1, data2, classes):
     #shuffle data
-    indicies = np.arange(data.shape[0])
+    #print(data1.shape)
+    #print(data2.shape)
+    indicies = np.arange(data1.shape[0])
     np.random.shuffle(indicies)
-    data = data[indicies]
+    data1 = data1[indicies]
+    data2 = data2[indicies]
     classes = classes[indicies]
-    return data, classes
+    return data1, data2, classes
 
 
 for file in os.listdir(directory):
@@ -97,15 +122,28 @@ for file in os.listdir(directory):
         saveName = filename.split(".")
         imgName = saveName[0]
         className = imgName.split("_")
-        className = className[1] 
+        className = className[2] 
+        #print(className)
         df = pd.read_pickle(dataDir + filename)
-        x_test = reshapeData(df)
-        x_test = np.concatenate((x_test[0], x_test[1], x_test[2]))
+        x_orig = reshapeData(df)
+        x_clean = clean(x_orig)
+        x_orig = reshapeData(df)
+        x_orig = np.concatenate((x_orig[0], x_orig[1], x_orig[2]))
+        x_clean = np.concatenate((x_clean[0], x_clean[1], x_clean[2]))
+        fig, plot = plt.subplots(2,5, figsize = (8,5))
+        #for j in range(5):
+            #rand = np.random.randint(0, x_clean.shape[0])
+            #plot[0,j].imshow(x_orig[rand, :, :, 0])
+            #plot[1,j].imshow(x_clean[rand, :, :, 0])
+        #plt.savefig(workDir + 'TestImg.png')
+        print("clean shape", x_clean.shape)
+        print("orig shape", x_orig.shape)
         att = getAttributes(df)
-        data, classes = shuffleData(x_test, att)
-        np.save(saveDir + imgName + '_Clean.npy', data)
-        np.save(saveDir + 'classes' + className + '_Clean.npy', classes)
-        
+        data_clean, data_orig, classes = shuffleData(x_clean, x_orig,  att)
+        np.save(saveDir + imgName + '_Clean.npy', data_clean)
+        #np.save(saveDir + imgName + '_Orig.npy', data_orig)
+        np.save(saveDir + 'classes_0p25' + className + '_Clean.npy', classes)
+        print("Files saved")
         
 
 
