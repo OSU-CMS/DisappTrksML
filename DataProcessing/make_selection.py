b7e67962e4ca4344c2977a94013be532f981c456
lucalavezzo <38077849+lucalavezzo@users.noreply.github.com>
2020-08-11 11:01:11 -0400

    condorized selection

--- DataProcessing/make_selection.py
+++ DataProcessing/make_selection.py
@@ -3,9 +3,8 @@ import numpy as np
 import sys
 import json
 import math
-import pickle
 
-dataDir = "/data/disappearingTracks/converted_DYJetsToLL_M50/"
+dataDir = "/store/user/llavezzo/disappearingTracks/converted_DYJetsToLL_M50/"
 tag = "0p25_"
 signal = "e"					#choose: e, m, bkg
 
@@ -20,83 +19,73 @@ infos:
 5: deltaRToClosestTauHaud
 
 """
-def save_obj(obj, name ):
-    with open(name + '.pkl', 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
+if(signal == "e"):
+	signal_index = [0]
+	bkg_index = [1,2]
+	reco_index = 3
+if(signal == "m"):
+	signal_index = [1]
+	bkg_index = [0,2]
+	reco_index = 4
 
-sCounts = {}
-bkgCounts = {}
+# script arguments
+process = int(sys.argv[1])
+print("Process",process)
+# name of the file to import
+files = np.load('fileslist.npy')
+fileNum = files[process]
 
-for fileNum in range(0,4000):
+e_fname = 'images_e_'+tag+str(fileNum)+'.npz'
+m_fname = 'images_m_'+tag+str(fileNum)+'.npz'
+bkg_fname = 'images_bkg_'+tag+str(fileNum)+'.npz'
 
-	if(signal == "e"):
-		signal_index = [0]
-		bkg_index = [1,2]
-		reco_index = 3
-	if(signal == "m"):
-		signal_index = [1]
-		bkg_index = [0,2]
-		reco_index = 4
+# check if file exists
+if(not os.path.isfile(dataDir+bkg_fname)): sys.exit(0)
 
-	e_fname = 'images_e_'+tag+str(fileNum)+'.npz'
-	m_fname = 'images_m_'+tag+str(fileNum)+'.npz'
-	bkg_fname = 'images_bkg_'+tag+str(fileNum)+'.npz'
+# import e, m, bkg files containing images and infos
+temp1 = np.load(dataDir+e_fname)
+temp2 = np.load(dataDir+m_fname)
+temp3 = np.load(dataDir+bkg_fname)
 
-	# check if file exists
-	if(not os.path.isfile(dataDir+bkg_fname)): continue
+infos = np.asarray([temp1['infos'],temp2['infos'],temp3['infos']])
+images = np.asarray([temp1['images'],temp2['images'],temp3['images']])
 
-	# import e, m, bkg files containing images and infos
-	temp1 = np.load(dataDir+e_fname)
-	temp2 = np.load(dataDir+m_fname)
-	temp3 = np.load(dataDir+bkg_fname)
+# join the files based on classification 
+s_images = images[signal_index]
+s_infos = infos[signal_index]
+bkg_images = images[bkg_index]
+bkg_infos = infos[bkg_index]
 
-	infos = np.asarray([temp1['infos'],temp2['infos'],temp3['infos']])
-	images = np.asarray([temp1['images'],temp2['images'],temp3['images']])
+s_images = np.vstack(s_images)
+s_infos = np.vstack(s_infos)
+bkg_images = np.vstack(bkg_images)
+bkg_infos = np.vstack(bkg_infos)	
 
-	# join the files based on classification 
-	s_images = images[signal_index]
-	s_infos = infos[signal_index]
-	bkg_images = images[bkg_index]
-	bkg_infos = infos[bkg_index]
+# select signal reco fail, convert to tanh
+s_outImages, s_outInfos = [],[]
+for info, image in zip(s_infos, s_images):
+	if(math.fabs(info[reco_index]) > 0.15):
+		s_outImages.append(image)
+		s_outInfos.append(info)
 
-	s_images = np.vstack(s_images)
-	s_infos = np.vstack(s_infos)
-	bkg_images = np.vstack(bkg_images)
-	bkg_infos = np.vstack(bkg_infos)	
+# select signal reco fail, convert to tanh
+bkg_outImages, bkg_outInfos = [],[]
+for info, image in zip(bkg_infos, bkg_images):
+	if(math.fabs(info[reco_index]) > 0.15):
+		bkg_outImages.append(image)
+		bkg_outInfos.append(info)
 
-	# select signal reco fail, convert to tanh
-	s_outImages, s_outInfos = [],[]
-	for info, image in zip(s_infos, s_images):
-		if(math.fabs(info[reco_index]) > 0.15):
-			s_outImages.append(image)
-			s_outInfos.append(info)
+# some checks before saving
+assert len(s_outImages)==len(s_outInfos)
+assert len(bkg_outImages)==len(bkg_outInfos)
 
-	# select signal reco fail, convert to tanh
-	bkg_outImages, bkg_outInfos = [],[]
-	for info, image in zip(bkg_infos, bkg_images):
-		if(math.fabs(info[reco_index]) > 0.15):
-			bkg_outImages.append(image)
-			bkg_outInfos.append(info)
+print("File",fileNum)
+print("Saving",len(s_outImages),len(bkg_outImages),"from",len(s_images)+len(bkg_images),"files")
+print()
 
-	# some checks before saving
-	assert len(s_outImages)==len(s_outInfos)
-	assert len(bkg_outImages)==len(bkg_outInfos)
-
-	sCounts.update({fileNum:len(s_outImages)})
-	bkgCounts.update({fileNum:len(bkg_outImages)})
-
-	print("File",fileNum)
-	print("Saving",len(s_outImages),len(bkg_outImages),"from",len(s_images)+len(bkg_images),"files")
-	print()
-
-	# save files	
-	outDir = '/data/disappearingTracks/out/'
-	f1 = signal+'_'+tag+str(fileNum)
-	f2 = 'bkg_'+tag+str(fileNum)
-
-	np.save(outDir+f1,s_outImages)
-	np.save(outDir+f2,bkg_outImages)
-
-save_obj(sCounts, "sCounts.pkl")
-save_obj(bkgCounts, "bkgCounts.pkl")
+# save files	
+f1 = signal+'_'+tag+str(fileNum)
+f2 = 'bkg_'+tag+str(fileNum)
+np.save(f1,s_outImages)
+np.save(f2,bkg_outImages)
--- DataProcessing/selection_wrapper.sh
+++ DataProcessing/selection_wrapper.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 python make_selection.py $1
-mv images*.npy /store/user/llavezzo/disappearingTracks/electron_selection_noscaling/
