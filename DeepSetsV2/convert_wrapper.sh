d8ecfebc99cfd795c8ebf8b9115b689259fe4d4e
lucalavezzo <38077849+lucalavezzo@users.noreply.github.com>
2020-09-15 16:11:35 -0400

    minor convert data update

--- DeepSetsV2/convert_wrapper.sh
+++ DeepSetsV2/convert_wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python convertData.py $1
-mv *.npz /store/user/llavezzo/disappearingTracks/converted_deepSets100_failAllRecos/
+mv *.npz /store/user/llavezzo/disappearingTracks/converted_deepSets100_Zee_failAllRecos/
--- DeepSetsV2/count.py
+++ DeepSetsV2/count.py
@@ -3,9 +3,7 @@ import numpy as np
 import sys
 import pickle as pkl
 
-dataDir = "/store/user/llavezzo/disappearingTracks/converted_deepSets100_Zee_failAllRecos/"
-
-tag = "_0p5"
+dataDir = "/store/user/llavezzo/disappearingTracks/converted_deepSets100_Zee/"
 
 eCounts = {}
 bkgCounts = {}
@@ -13,23 +11,22 @@ electrons, bkg = 0, 0
 
 for file in os.listdir(dataDir):
 
-	if("images"+tag not in file): continue 
+	if("images" not in file): continue 
 	if(".npz" not in file): continue 
 
 	index = file.find(".")
-	fileNum = file[11:index]
-	print(fileNum)
+	fileNum = int(file[11:index])
 
 	temp1 = np.load(dataDir+file)['e']
+	np.save(dataDir+"e_0p5_"+str(fileNum)+".npy",temp1)
 	eCounts.update({fileNum:temp1.shape[0]})
 	electrons += temp1.shape[0]
 
 	temp2 = np.load(dataDir+file)['bkg']
+	np.save(dataDir+"bkg_0p5_"+str(fileNum)+".npy",temp2)
 	bkgCounts.update({fileNum:temp2.shape[0]})
 	bkg += temp2.shape[0]
-
-print(electrons)
-print(bkg)
+	
 with open(dataDir+'eCounts.pkl', 'wb') as f:
 	pkl.dump(eCounts,f)
 with open(dataDir+'bkgCounts.pkl', 'wb') as f:
