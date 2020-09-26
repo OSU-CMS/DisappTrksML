ffe3ee401b8facede7136b16ad68283e87100899
lucalavezzo <38077849+lucalavezzo@users.noreply.github.com>
2020-09-26 09:45:40 -0400

    minor cleaning

--- DeepSetsV2/validate.py
+++ DeepSetsV2/validate.py
@@ -41,14 +41,13 @@ def run_batch_validation(model, weights, batchDir, dataDir, plotDir, batch_size)
 	for t,pred,index in zip(true,predictions, indices):
 		if(pred[1]>0.5):
 			if(t[1]>0.5): 
-				cm[1][1]+=1;
+				cm[1][1]+=1
 			else: 
-				cm[1][0]+=1;			
+				cm[1][0]+=1		
 		else:
 			if(t[1]>0.5): 
-				cm[0][1]+=1;
-			else: cm[0][0]+=1;
-
+				cm[0][1]+=1
+			else: cm[0][0]+=1
 	print(cm)
 
 	utils.metrics(true[:,1], predictions[:,1], plotDir, threshold=0.5)
@@ -57,12 +56,12 @@ def run_batch_validation(model, weights, batchDir, dataDir, plotDir, batch_size)
 	print(utils.bcolors.GREEN+"Saved metrics to "+plotDir+utils.bcolors.ENDC)
 	print()
 
-	np.savez_compressed("validation_outputs",
+	np.savez_compressed(batchDir+"validation_outputs",
 						truth = true,
 						predicted = predictions,
 						indices = indices)
 
-def run_validation(model, weights, dataDir):
+def run_validation(model, weights, dataDir,plotDir=""):
 	print("------------------STARTING VALIDATION--------------------")
 	model.load_weights(weights)
 
@@ -75,11 +74,29 @@ def run_validation(model, weights, dataDir):
 
 		data = np.load(dataDir+file)
 		events = data['e'][:,2:]
+		indices = data['e'][:,:2]
 		events = np.reshape(events,(len(events),100,4))
 		infos = data['e_infos']
 
-		predictions.append(model.predict(events))
-		predictions_eReco.append(infos[:,4])
+		preds = model.predict(events)
+		recos = infos[:,4]
+		predictions.append(preds)
+		predictions_eReco.append(recos)
+
+		# done1, done2, done3, done4 = False, False, False, False
+		# for e,i,p,reco in zip(events,indices,preds,recos):
+		# 	if(p[1] >= 0.5 and reco < 0.15 and not done1):
+		# 		utils.save_event_deepSets(e,plotDir+"e_identified_reco/"+str(i[0])+"_"+str(i[1])+".png")
+		# 		done1 = True
+		# 	if(p[1] >= 0.5 and reco >= 0.15 and not done2):
+		# 		utils.save_event_deepSets(e,plotDir+"e_identified_nonreco/"+str(i[0])+"_"+str(i[1])+".png")
+		# 		done2 = True
+		# 	if(p[1] < 0.5 and reco < 0.15 and not done3):
+		# 		utils.save_event_deepSets(e,plotDir+"e_missed_reco/"+str(i[0])+"_"+str(i[1])+".png")
+		# 		done3 = True
+		# 	if(p[1] < 0.5 and reco >= 0.15 and not done4):
+		# 		utils.save_event_deepSets(e,plotDir+"e_missed_nonreco/"+str(i[0])+"_"+str(i[1])+".png")
+		# 		done4 = True
 
 		print(file)
 
@@ -89,11 +106,10 @@ def run_validation(model, weights, dataDir):
 
 if __name__ == "__main__":
 
-	dataDir = "/store/user/llavezzo/disappearingTracks/converted_deepSets100_realEle/"
-	batchDir = "/data/users/llavezzo/deepSets100_2/deepSets100_2_p0/outputFiles/"
-	plotDir = "/data/users/llavezzo/deepSets100_2/deepSets100_2_p0/"
-
-	weights = "/data/users/llavezzo/deepSets100_3/deepSets100_3_p0/weights/lastEpoch.h5"
+	dataDir = "/store/user/llavezzo/disappearingTracks/converted_deepSets100_Zee_V3/"
+	batchDir = "deepSets_2/outputFiles/"
+	plotDir = "deepSets_2/plots/"
+	weights = "deepSets_2/weights/lastEpoch.h5"
 
 	model = buildModel()
 
@@ -101,5 +117,5 @@ if __name__ == "__main__":
 				  loss='categorical_crossentropy', 
 				  metrics=['accuracy'])
 
-	# run_batch_validation(model, weights, batchDir, dataDir, plotDir, 64)
-	run_validation(model,weights,dataDir)
+	run_batch_validation(model, weights, batchDir, dataDir, plotDir, 128)
+	# run_validation(model,weights,dataDir,plotDir)
