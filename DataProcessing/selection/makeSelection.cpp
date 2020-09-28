979ddd4d2f99285fd1d174121f14e7a58ac323dc
Luca Lavezzo <llavezzo@interactive-0-0.local>
2020-09-28 18:45:50 -0400

    workng version of makeSelection.cpp

--- DataProcessing/selection/makeSelection.cpp
+++ DataProcessing/selection/makeSelection.cpp
@@ -1,4 +1,5 @@
 #include "DisappTrksML/TreeMaker/interface/Infos.h"
+#include "DataFormats/Math/interface/deltaR.h"
 
 TString string_tstring(string thing){
 	TString thingT = thing;
@@ -21,26 +22,20 @@ bool passesSelection(TrackInfo track){
 	return true;
 }
 
-bool check_ZtoEE(vector<TrackInfo> tracks){
-	int count(0);
-	for(auto const t : tracks){
-		if(!passesSelection(track)) continue
-		if(isGenMatched(track,11)) count+=1;
-	}
-	if(count>=2) return true;
-	else return false;
-}
-
 bool isReconstructed(TrackInfo track, string flavor){
 	if(flavor == "electron") return abs(track.deltaRToClosestElectron) < 0.15;
-	else if(flavor == "muon")return abs(track.deltaRToClosestMuon) < 0.15;
-	else if(flavor == "tau")return abs(track.deltaRToClosestTauHad) < 0.15;
+	else if(flavor == "muon") return abs(track.deltaRToClosestMuon) < 0.15;
+	else if(flavor == "tau") return abs(track.deltaRToClosestTauHad) < 0.15;
 	else return false;
 }
 
 void makeSelection(int file = 0, TString dataDir = "", TString outDir = ""){
 
+	// parameters
 	dataDir = "/store/user/bfrancis/images_DYJetsToLL_v3/";
+	bool Zee = false;
+
+	const double minGenParticlePt_ = 10;
 
 	TString oldFileName = dataDir+"hist_"+int_tstring(file)+".root";
 	TFile * oldFile = TFile::Open(oldFileName, "read");
@@ -59,27 +54,68 @@ void makeSelection(int file = 0, TString dataDir = "", TString outDir = ""){
 
 	TString newFileName = outDir+"hist_"+int_tstring(file)+".root";
 	TFile * newFile = new TFile(newFileName, "recreate");
-	TTree * newTree = oldTree->CloneTree(0);
+	TTree * eTree = new TTree("eTree","eTree");
+	TTree * bTree = new TTree("bTree","bTree");
+	std::vector<TrackInfo> * v_tracks_e = new std::vector<TrackInfo>();
+	std::vector<TrackInfo> * v_tracks_b = new std::vector<TrackInfo>();
+	eTree->Branch("nPV",nPV);
+	eTree->Branch("recHits",&v_recHits);
+	eTree->Branch("tracks",&v_tracks_e);
+	bTree->Branch("nPV",nPV);
+	bTree->Branch("recHits",&v_recHits);
+	bTree->Branch("tracks",&v_tracks_b);
 
 	cout << "Running over " << oldFileName << " with " << to_string(oldTree->GetEntries()) << " events." << endl;
 
 	for(int iE = 0; iE < oldTree->GetEntries(); iE++){
+
+		v_tracks_b->clear();
+		v_tracks_e->clear();
+
 		oldTree->GetEntry(iE);
-		if(iE > 100) break; //debug
 
 		// make selections
-		for(int iT = 0; iT < (*v_tracks).size(); iT++){
+		for(const auto &track : *v_tracks){
 
-			if(!passesSelection((*v_tracks)[iT])) continue;
+			// selections
+			if(!passesSelection(track)) continue;
 
-			if(isReconstructed((*v_tracks)[iT],"muon")) continue;
-			if(isReconstructed((*v_tracks)[iT],"tau")) continue;
+			if(isReconstructed(track,"muon")) continue;
+			if(isReconstructed(track,"tau")) continue;
 
+			// gen matching
+			int genMatchedID = 0;
+			double genMatchedDR(-1), genMatchedPt(-1);
+			int genMatchedID_promptFinalState = 0;
+			double genMatchedDR_promptFinalState(-1), genMatchedPt_promptFinalState(-1);
+			for(const auto &genParticle : *v_genParticles) {
+				if(genParticle.pt < minGenParticlePt_) continue;
+				double thisDR = deltaR(genParticle.eta,genParticle.phi,track.eta,track.phi);
+				if(genMatchedDR < 0 || thisDR < genMatchedDR) {
+					genMatchedDR = thisDR;
+					genMatchedID = genParticle.pdgId;
+					genMatchedPt = genParticle.pt;
+					if(genParticle.isPromptFinalState || genParticle.isDirectPromptTauDecayProductFinalState) {
+						genMatchedDR_promptFinalState = thisDR;
+						genMatchedID_promptFinalState = genParticle.pdgId;
+						genMatchedPt_promptFinalState = genParticle.pt;
+					}
+				}
+			}
+			if(abs(genMatchedID) == 11 and abs(genMatchedDR) < 0.1) v_tracks_e->push_back(track);
+			else v_tracks_b->push_back(track);
 		}
-		newTree->Fill();
+
+		if(Zee){
+			if(v_tracks_e->size() < 2) continue;
+		}
+		if(v_tracks_e->size() > 0) eTree->Fill();
+		if(v_tracks_b->size() > 0) bTree->Fill();
 	}
 
-	cout << "Saving file " << newFileName << " with " << newTree->GetEntries() << " events." << endl;
+	cout << "Saving file " << newFileName << " with" << endl;
+	cout << eTree->GetEntries() << " electron tracks and" << endl;
+	cout << bTree->GetEntries() << " background tracks." << endl;
 
 	newFile->Write();
 }
