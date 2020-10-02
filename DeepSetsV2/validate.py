d5918fecdc281ac6710f34f6f0c0fc5b7aa3ede7
lucalavezzo <38077849+lucalavezzo@users.noreply.github.com>
2020-10-02 10:37:31 -0400

    updated vadliation script

--- DeepSetsV2/validate.py
+++ DeepSetsV2/validate.py
@@ -15,8 +15,44 @@ import utils
 from generator import generator
 from model import buildModel, buildModelWithEventInfo
 
+def run_batch_validation(model, weights, batchDir, dataDir, plotDir):
+	print("------------------STARTING VALIDATION--------------------")
+	model.load_weights(weights)
 
-def run_batch_validation(model, weights, batchDir, dataDir, plotDir, batch_size):
+	# load the batches used to train and validate
+	val_e_file_batches = np.load(batchDir+'e_files_valBatches.npy', allow_pickle=True)
+	val_e_event_batches = np.load(batchDir+'e_events_valBatches.npy', allow_pickle=True)
+	val_bkg_file_batches = np.load(batchDir+'bkg_files_valBatches.npy', allow_pickle=True)
+	val_bkg_event_batches = np.load(batchDir+'bkg_events_valBatches.npy', allow_pickle=True)
+	file_batches = np.concatenate((val_e_file_batches,val_bkg_file_batches))
+	event_batches = np.concatenate((val_e_event_batches,val_bkg_event_batches))
+	class_labels = np.concatenate((['e']*val_e_file_batches.shape[0],['bkg']*val_bkg_file_batches.shape[0]))
+
+	predictions, infos = [],[]
+	for events,files,class_label in zip(event_batches,file_batches,class_labels):
+		events = generator.load_data(files,events,class_label,dataDir)
+		batch_infos = generator.load_data(files,events,class_label+'_infos',dataDir)
+
+		events = events[:,3:]
+		events = np.reshape(events,(events.shape[0],100,4))
+
+		preds = model.predict(events)
+		predictions.append(preds)
+		infos.append(batch_infos)
+
+	utils.metrics(true[:,1], predictions[:,1], plotDir, threshold=0.5)
+
+	print()
+	print(utils.bcolors.GREEN+"Saved metrics to "+plotDir+utils.bcolors.ENDC)
+	print()
+
+	np.savez_compressed(batchDir+"validation_outputs",
+						truth = true,
+						predicted = predictions,
+						indices = indices)
+
+
+def run_batch_validation2(model, weights, batchDir, dataDir, plotDir, batch_size):
 	print("------------------STARTING VALIDATION--------------------")
 	model.load_weights(weights)
 
@@ -65,8 +101,7 @@ def run_validation(model, weights, dataDir,plotDir=""):
 	print("------------------STARTING VALIDATION--------------------")
 	model.load_weights(weights)
 
-	predictions = []
-	predictions_eReco = []
+	predictions, infos = [], []
 
 	for file in os.listdir(dataDir):
 		if(".npz" not in file): continue
@@ -76,12 +111,11 @@ def run_validation(model, weights, dataDir,plotDir=""):
 		events = data['e'][:,2:]
 		indices = data['e'][:,:2]
 		events = np.reshape(events,(len(events),100,4))
-		infos = data['e_infos']
+		batch_infos = data['e_infos']
 
 		preds = model.predict(events)
-		recos = infos[:,4]
 		predictions.append(preds)
-		predictions_eReco.append(recos)
+		infos.append(batch_infoss)
 
 	np.savez_compressed("validation_outputs",
 						pred = predictions,
@@ -102,3 +136,5 @@ if __name__ == "__main__":
 
 	run_batch_validation(model, weights, batchDir, dataDir, plotDir, 128)
 	# run_validation(model,weights,dataDir,plotDir)
+
+
