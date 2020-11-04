bb5faf30b79b35740ad914ae225e9fafba01f3cb
Brian Francis <brian.patrick.francis@cern.ch>
2020-11-04 15:23:12 -0500

    Fix generator class for input dataset

--- DeepSets/python/generator.py
+++ DeepSets/python/generator.py
@@ -3,17 +3,50 @@ from tensorflow import keras
 
 class DataGenerator(keras.utils.Sequence):
 
-	def __init__(self, file_ids, inputDir='.', batch_size=32, dim=(100,4), n_classes=2, shuffle=True):
+	def __init__(self, 
+				 file_ids, 
+				 input_dir='.', 
+				 batch_size=32, 
+				 dim=(100,4), 
+				 n_classes=2, 
+				 shuffle=True):
 		self.file_ids = file_ids
-		self.inputDir = inputDir
+		self.input_dir = input_dir
 		self.batch_size = batch_size
 		self.dim = dim
 		self.n_classes = n_classes
 		self.shuffle = shuffle
+
+		self.signal_data = None
+		self.background_data = None
+		self.num_signal = 0
+		self.num_background = 0
+
+		self.load_data()
 		self.on_epoch_end()
 
+	def load_data(self):
+		for idx in self.file_ids:
+			fin = np.load(self.input_dir + '/hist_' + idx + '.root.npz')
+			if self.signal_data is None and self.background_data is None:
+				self.signal_data = fin['signal']
+				self.background_data = fin['background']
+			else:
+				self.signal_data = np.vstack((self.signal_data, fin['signal']))
+				self.background_data = np.vstack((self.background_data, fin['background']))
+		self.num_signal = self.signal_data.shape[0]
+		self.num_background = self.background_data.shape[0]
+
+		print 'Ay got:'
+		print '\t', self.num_signal, 'signal events'
+		print '\t', self.background_data, 'background events'
+		print 'shapes: signal_data =', self.signal_data.shape
+		print '        background_data =', self.background_data.shape
+
 	def __len__(self):
-		return int(np.floor(len(self.file_ids) / self.batch_size))
+		max_signal_batches = np.floor(self.signal_data / self.batch_size)
+		max_background_batches = np.floor(self.background_data / self.batch_size)
+		return int(min(max_signal_batches, max_background_batches))
 
 	def __getitem__(self, index):
 		indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
@@ -22,20 +55,16 @@ class DataGenerator(keras.utils.Sequence):
 		return X, y
 
 	def on_epoch_end(self):
-		self.indexes = np.arange(len(self.file_ids))
 		if self.shuffle:
-			np.random.shuffle(self.indexes)
+			np.random.shuffle(self.signal_data)
+			np.random.shuffle(self.background_data)
 
-	def __data_generation(self, file_indices):
-		X = np.empty((self.batch_size, self.dim[0], self.dim[1]))
-		y = np.empty((self.batch_size), dtype=int)
+	def __data_generation(self, index):
+		X = self.signal_data[index * self.batch_size : (index + 1) * self.batch_size]
+		y = np.ones(self.batch_size)
 
-		# generate data
-		for idx in file_indices:
-			fin = np.load(self.inputDir + '/hist_' + idx + '.root.npz')
-			X = np.concatenate((X, fin['signal']))
-			X = np.concatenate((X, fin['background']))
-			y = np.concatenate((y, np.ones(len(fin['signal'])), np.ones(len(fin['background']))))
+		X = np.vstack((X, self.background_data[index * self.batch_size : (index + 1) * self.batch_size]))
+		y = np.concatenate((y, np.zeros(self.batch_size)))
 
 		p = np.random.permutation(len(X))
 		X = X[p]
