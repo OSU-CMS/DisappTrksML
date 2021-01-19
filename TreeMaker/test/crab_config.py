1ea0b6ca12613de93a77c5035fd4deb87749977f
Michael Carrigan <carriganm95@gmail.com>
2021-01-19 10:27:54 -0500

    added crab config file for processing data

---
+++ TreeMaker/test/crab_config.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'crab'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'treeMaker_MC2017_cfg.py'  # For MC only
+config.JobType.allowUndistributedCMSSW = True
+
+config.JobType.numCores = 4
+config.JobType.maxMemoryMB = 2500
+config.Data.inputDataset = ''
+config.Data.useParent = False
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'LumiBased' # for both MC and data
+config.Data.outLFNDirBase = '/store/user/micarrig/'
+config.Data.publication = True
+config.Data.outputDatasetTag = ''
+
+config.Site.storageSite = 'T3_US_FNALLPC'
+
+if __name__ == '__main__':
+
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+    from multiprocessing import Process
+
+    def submit(config):
+        try:
+            crabCommand('submit', config = config)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+
+    def forkAndSubmit(config):
+        p = Process(target=submit, args=(config,))
+        p.start()
+        p.join()
+
+
+    #############################################################################################
+    ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
+    #############################################################################################
+
+    #############################################################################################
+    ## Data
+    #############################################################################################
+
+    #config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'
+
+    # The AOD re-reco is labeled 17Nov17, however the re-reco campaign is labeled 31Mar2018
+
+    # Run2017F-v1
+
+    config.Data.outputDatasetTag = 'Images-v6-DYJets-MC2017'
+    config.Data.unitsPerJob = 54 # 61275 lumis
+
+    config.General.requestName = 'candidateTrackProducer_v6_DYJetsToLL_M-50_MC2017'
+    config.Data.inputDataset   = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17DRPremix-RECOSIMstep_94X_mc2017_realistic_v10-v1/AODSIM'
+    config.JobType.psetName    = 'treeMaker_MC2017_cfg.py'
+    forkAndSubmit(config)
