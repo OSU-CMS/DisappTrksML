c5ac6c946b8ec3fcc3aacace101743171dcd616b
Kai Wei <czkaiweb@gmail.com>
2021-01-22 13:42:44 +0100

    sample configuration file added

---
+++ Configuration/python/ConvertedSamples.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+############################################################################################################
+#########  LIST OF NumPy SAMPLES  ##########################################################################
+############################################################################################################
+
+dataset_name = {
+	#v5
+	'DYJetsToLL_RecoMuon_v5'		:	'/store/user/llavezzo/disappearingTracks/images_DYJetsToLL_v5_muons/',
+	'DYJetsToLL_RecoIncBkg_v5'	    :	'',
+	'DYJetsToLL_RecoElectron_v5'	:	'',
+	'DYJetsToLL_GenTagMuon_v5'	    :	'',
+	'DYJetsToLL_GenTagIncBkg_v5'	:	'',
+}
