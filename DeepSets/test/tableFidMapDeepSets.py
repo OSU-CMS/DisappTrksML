69ffeb0f5acc14c8bd2b96c72ca02fdbb0bf4cfe
lucalavezzo <38077849+lucalavezzo@users.noreply.github.com>
2021-05-02 11:06:25 -0400

    delete the condorized evaluation script (not used anymore), added comments and cleaned up table making script

--- DeepSets/test/tableFidMapDeepSets.py
+++ DeepSets/test/tableFidMapDeepSets.py
@@ -1,3 +1,8 @@
+"""
+Prints a LaTex table to compare number of events passing
+fiducial map and deepSets, and ratio of efficiencies.
+"""
+
 import ROOT as r
 import numpy as np 
 import math
@@ -11,12 +16,14 @@ def findSigFigIndex(x):
 	if x == 0: return 0
 	return -int(math.floor(math.log10(abs(x))))
 
+# load the fiducial map sigmas for each data sample
 ele_fidMap = np.load("ele_fidMap.npy", allow_pickle=True, encoding="bytes")
 h10_fidMap = np.load("h10_fidMap.npy", allow_pickle=True, encoding="bytes")
 h100_fidMap = np.load("h100_fidMap.npy", allow_pickle=True, encoding="bytes")
 h1000_fidMap = np.load("h1000_fidMap.npy", allow_pickle=True, encoding="bytes")
 h10000_fidMap = np.load("h10000_fidMap.npy", allow_pickle=True, encoding="bytes")
 
+# load the deepSets predictions for each data sample
 ele_preds = np.load("SingleEle_fullSel_preds.npy", allow_pickle=True, encoding="bytes")
 h10_preds = np.load("h10_fullSel_preds.npy", allow_pickle=True, encoding="bytes")
 h100_preds = np.load("h100_fullSel_preds.npy", allow_pickle=True, encoding="bytes")
@@ -30,12 +37,15 @@ labels = [r"Tag \& Probe Electrons ",
 		"Higgsino 700 GeV, 10000cm"]
 totals = []
 npass_clf, totals, npass_fidMap = [], [], []
+
+# cut on deepSets discriminant value disc_values
 disc_vaue = 0.5
 for file in [ele_preds, h10_preds, h100_preds, h1000_preds, h10000_preds]:
 	totals.append(len(file))
 	n = len(file[file<disc_vaue])
 	npass_clf.append(n)
 
+# cut on fiducial map sigma < 2
 for file in [ele_fidMap, h10_fidMap, h100_fidMap, h1000_fidMap, h10000_fidMap]:
 	n = len(file[file<2])
 	npass_fidMap.append(n)
@@ -53,26 +63,10 @@ for i in range(len(totals)):
 	n = npass_clf[i]
 	s = np.sqrt(n)
 	row += str(int(n))
-	# if n == 0:
-	# 	row += "0^{{+1.1}}_{{-0}}"
-	# elif n+s >= totals[i]:
-	# 	s_up = totals[i]-n
-	# 	s_down = s
-	# 	row += r"{0}^{{+{1}}}_{{-{2}}}".format(round(n, findSigFigIndex(s)), round1(s_up), round1(s_down))
-	# else:
-	# 	row += r"{0}\pm{1}".format(round(n, findSigFigIndex(s)), round1(s))
 	row += " & "
 	n = npass_fidMap[i]
 	s = np.sqrt(n)
 	row += str(int(n))
-	# if n == 0:
-	# 	row += "0^{{+1.1}}_{{-0}}"
-	# elif n+s >= totals[i]:
-	# 	s_up = totals[i]-n
-	# 	s_down = s
-	# 	row += r"{0}^{{+{1}}}_{{-{2}}}".format(round(n, findSigFigIndex(s)), round1(s_up), round1(s_down))
-	# else:
-	# 	row += r"{0}\pm{1}".format(round(n, findSigFigIndex(s)), round1(s))
 	row += " & "
 	n = ratios[i]
 	s = ratio_sigmas[i]
@@ -82,18 +76,3 @@ for i in range(len(totals)):
 		row += r"{0}\pm{1}".format(round(n, findSigFigIndex(s)), round1(s))
 	row += r" \\"
 	print row
-
-# for n, total in zip(npass, totals):
-# 	hpass_clf = r.TH1D("hpass_clf", "hpass_clf", 1, 0, 1);
-# 	htotal = r.TH1D("htotal", "htotal", 1, 0, 1);
-# 	hpass_clf.Sumw2()
-# 	htotal.Sumw2()
-# 	for i in range(n): hpass_clf.Fill(0.5)
-# 	for i in range(total): htotal.Fill(0.5)
-# 	teff_clf = r.TEfficiency(hpass_clf, htotal)
-# 	eff = teff_clf.GetEfficiency(1)
-# 	eff_error_up = teff_clf.GetEfficiencyErrorUp(1)
-# 	eff_error_down = teff_clf.GetEfficiencyErrorLow(1)
-# 	del hpass_clf, htotal, teff_clf
-
-# 	print r""+str(eff)+"^{+"+str(round1(eff_error_up))+"}_{-"+str(round1(eff_error_down))+"}"
