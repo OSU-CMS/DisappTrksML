924bbeff4f886fcafc6230d55e1fa4bd0449eb7c
Michael Carrigan <carriganm95@gmail.com>
2022-07-21 14:16:45 -0400

    changes to make singularity run in /data1/mcarrigan/tmp

--- Singularity/build_container.sh
+++ Singularity/build_container.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 
-export TMPDIR=/data1/tmp/
-export SINGULARITY_CACHEDIR=/data1/tmp/
+if [ ! -d "/data1/$USER/tmp" ]
+then 
+  mkdir /data1/$USER/tmp
+fi
+
+export TMPDIR=/data1/$USER/tmp/
+export SINGULARITY_CACHEDIR=/data1/$USER/tmp/
 
 singularity build --fakeroot --sandbox osuML build.def
