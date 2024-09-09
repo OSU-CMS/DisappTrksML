79f149893d75c47e7c7eba14f76990612f584aa0
ryansantos1174 <ryansantos1174@gmail.com>
2024-09-09 16:40:40 -0400

    Changing import paths and parameter formatting

--- MachineLearning/condor_runNN.py
+++ MachineLearning/condor_runNN.py
@@ -2,8 +2,8 @@ import numpy as np
 import subprocess
 
 training_parameters = np.array(
-    [{"phi_layers": ["layers", [32, 64, 128, 256]],
-      "f_layers": ["layers", [32, 64, 128, 256]]}]
+    [{"phi_layers": ["layers", 1, 5, [32, 64, 128, 256]],
+      "f_layers": ["layers", 1, 5, [32, 64, 128, 256]]}]
 )
 
 np.save('training_params', training_parameters)
--- MachineLearning/runNN.py
+++ MachineLearning/runNN.py
@@ -4,32 +4,41 @@ tune hyprparameters
 """
 
 import sys
+import os
 import pickle
 
 import numpy
 
 from networkController import NetworkController
-from DisappTrksML.DeepSets.python.ElectronModel import ElectronModel
+from ElectronModel import ElectronModel
 
 index = int(sys.argv[1])
 print("Index: ", index)
-controller = NetworkController(ElectronModel)
+model = ElectronModel() # Need this so can access class properties correctly
+
+controller = NetworkController(model)
 
 params = numpy.load("training_params.npy", allow_pickle=True)
 
-
-data_directory="/data"
-
+# If running in the singularity container, bind the directory you want to use for data
+# input to /data in the container.
+training_directory="/data/training/"
+testing_directory="/data/testing/"
+logging_directory="/store/"
 
 controller.tune_hyperparameters(trainable_params= params[index],
-                                train_parameters={"epochs" : 10,
-                                                  "val_generator_params": {"input_dir": data_directory,
+                                train_parameters={"epochs" : 1,
+                                                  "val_generator_params": {"input_dir": training_directory,
                                                                            "info_indices": [4,8,9,12],
                                                                            "batch_size": 256},
-                                                  "train_generator_params": {"input_dir": data_directory,
+                                                  "train_generator_params": {"input_dir": training_directory,
                                                                              "info_indices":[4,8,9,12],
+                                                                             "max_hits" : 100,
                                                                              "batch_size":256},
                                                   "use_tensorboard": True},
                                 build_parameters={},
-                                use_gpu=False,
-                                input_dir=data_directory)
+                                use_gpu=True,
+                                num_trials = 1,
+                                log_dir = logging_directory,
+                                input_dir=training_directory,
+                                testing_dir=testing_directory)
